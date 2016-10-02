3ffff712463009e7605bc63ff601cac8d3760514
shivam5 <2015csb1032@iitrpr.ac.in>
2016-10-02 14:09:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
-console.log('Loaded!');
+//Counter
+
+var button=getElementById("counter");
+
+var count=getElementById("count");
+
+var ctr=0;
+
+button.onclick=function(){
+    ctr=ctr+1;
+    count.InnerHtml=ctr;
+};
