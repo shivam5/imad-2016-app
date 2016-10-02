64264b6d819d964b742cc536cf7a45213b2c75b5
shivam5 <2015csb1032@iitrpr.ac.in>
2016-10-02 14:01:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ var counter =0;
 app.get('/counter', function(req,res){
     counter=counter+1;
     res.send (counter.toString());
-})
+});
 
 app.get('/:articleName', function(req, res){
 	var articleName=req.params.articleName;
